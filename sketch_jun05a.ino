2358d3719974ca7de77286fe04bb8b707783117f
Yash Raj Ghosh <75003551+yash-raj23@users.noreply.github.com>
2022-06-07 14:26:12 +0530

    Add files via upload

---
+++ sketch_jun05a.ino
@@ -0,0 +1,32 @@
+//#define trig=6;
+//#define echo=5;
+float duration, distance;
+void setup()
+{
+ Serial.begin(9600);
+ pinMode(6,OUTPUT);
+ pinMode(5,INPUT);
+}
+
+void loop()
+{
+ digitalWrite(6,LOW);
+ delayMicroseconds(2);
+ digitalWrite(6,HIGH);
+ delayMicroseconds(2);
+ 
+ duration=pulseIn(5,HIGH);
+ distance=(duration/2)*0.0343;
+ 
+ Serial.print("\ndistance= ");
+  if(distance>=400|| distance<=2)
+  {
+    Serial.print("*\nOUT OF RANGE*");
+  }
+  else{
+    Serial.print(distance);
+    Serial.print("cm");
+    delay(500);
+      }
+ delay(5000);
+}
